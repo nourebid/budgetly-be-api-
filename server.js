d5ba9140bf095bca014c52b024b6bdaeae59bfcb
nourebid <nour.ebid@gmail.com>
2020-07-24 17:02:18 +0200

    finalize the signin endpoint

--- server.js
+++ server.js
@@ -68,8 +68,23 @@ app.get('/', (req, res) => {
 
 
 app.post('/signin', (req, res) => {
-
-
+//new code
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+               return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('Unable to get user'))
+            } else {
+                res.status(400).json('Wrong Credentials please check your login email or password')
+            }
+        })
+        .catch(err => res.status(400).json('Wrong Credentials for login'))
     //old code
     // if (req.body.email === database.users[0].email && 
     //     req.body.password === database.users[0].password) {
