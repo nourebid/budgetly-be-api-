697756c31704038ac82aeeb9c81f4c359e6a6c1a
nourebid <nour.ebid@gmail.com>
2020-07-24 18:39:22 +0200

    fixing bugs in register and makes validation layer in register and signin endpoints

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 const handleRegister = (req, res, db, bcrypt, saltRounds) => {
     const { firstName, lastName, email, password } = req.body;
     //new code to connect database and push in PGSQL database with bcrypt and trx
+    if ( !firstName || !lastName || !email || !password ) {
+        return res.status(400).json('Incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password, saltRounds);
     db.transaction(trx => {
         trx.insert({
